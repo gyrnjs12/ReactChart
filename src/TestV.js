626baf6455382e2bfba2df927367def0423d0d3a
HyoKwon <rlagyrnjs12@naver.com>
2020-10-18 03:17:56 +0900

    유튜브 검색 API 테스트

--- src/TestV.js
+++ src/TestV.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import YTsearch from 'youtube-api-search';
-
+// 유튜브 검색 API
+// 할당량 하루에 10,000 쿼리, 요청 한번에 100쿼리 사용
 const API_KEY = 'AIzaSyD2dVDkSFK2ogtAH7eGzY6nLa4yHfbRfbE';
 
 function TestV(props) {
   const initalState = { data: null, selectedVide: null, term: null };
   const [video, SetVideo] = useState(initalState);
-  YTsearch({ key: API_KEY, term: 'Dynamite' }, (data) => {
-    SetVideo({
-      data: data,
-      selectedVide: data[0],
-      term: 'Dynamite',
+  const btn = () => {
+    // term은 검색어
+    YTsearch({ key: API_KEY, term: 'Lovesick Girls' }, (data) => {
+      SetVideo({
+        data: data, // 검색 결과 상위 5개 불러옴
+        selectedVide: data[0], // 첫번째 검색결과
+        term: 'Lovesick Girls', // 검색어
+      });
     });
-  });
+    console.log('버튼클릭', video);
+  };
+
   console.log(video);
-  return <div>HELLO</div>;
+  return (
+    <div>
+      <button onClick={btn}>ㅇㅣ벤트버튼</button>
+    </div>
+  );
 }
 
 export default TestV;
