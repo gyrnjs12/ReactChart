5af82ecda81ece78c7328278e2da5cd284f4d343
HyoKwon <rlagyrnjs12@naver.com>
2020-10-24 03:09:46 +0900

    서버 배포 해보기

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 
 app.use(cors());
 app.use('/chart', api);
-
+app.get('/', (req, res) => {
+  res.send('HELLO MUSIC CHART!!!');
+});
 const port = process.env.PORT || 3002;
 app.listen(port, () => console.log(`Linstening on port ${port}`));
