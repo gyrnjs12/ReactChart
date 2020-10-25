7428b91af508a3cdcf2c50156fa86826943ca4b8
HyoKwon <rlagyrnjs12@naver.com>
2020-10-26 02:54:24 +0900

    모바일 버전 작업중(Todo: ChartItem 모바일 수정)

--- src/components/ChartTemplate.js
+++ src/components/ChartTemplate.js
@@ -3,24 +3,20 @@ import styled from 'styled-components';
 import { useMediaQuery } from 'react-responsive';
 
 const ChartTemplateBlock = styled.div`
-  width: ${({ isPc }) => (isPc ? `1200px` : `768px`)};
+  width: ${({ isMobile }) => (isMobile ? `100vw` : `67vw`)};
   height: 100vh;
-
-  margin-left: 270px;
-  margin-right: 270px;
-
+  margin-left: ${({ isMobile }) => (isMobile ? `0px` : `16.5vw`)};
   position: relative;
   background: #f6f6f6;
-
   display: flex;
   flex-direction: column;
 `;
 
 function ChartTemplate({ children }) {
-  const isPc = useMediaQuery({
-    query: '(min-width: 769px)',
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)',
   });
-  return <ChartTemplateBlock isPc={isPc}>{children}</ChartTemplateBlock>;
+  return <ChartTemplateBlock isMobile>{children}</ChartTemplateBlock>;
 }
 
 export default React.memo(ChartTemplate);
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,29 @@
 import React, { useContext } from 'react';
 import styled, { ThemeContext } from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 import NavItem from './NavItem';
 
 const NavBar = styled.div`
-  width: 10vw;
+  width: 100vw;
   height: 40px;
-  padding-left: 250px;
-  padding-right: 250px;
   position: fix;
   background: #393b44;
   display: flex;
 `;
 
+const Box = styled.div`
+  padding-left: ${({ isMobile }) => (isMobile ? `0px` : `15.5vw`)};
+`;
+
 function Nav({ children }) {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)',
+  });
   const themeContext = useContext(ThemeContext);
   return (
     <NavBar>
       {/* <NavItem text="HOME" /> */}
+      <Box isMobile={isMobile} />
       <NavItem color={themeContext.colors.MELON} text="MELON" />
       <NavItem color={themeContext.colors.GENIE} text="GENIE" />
       <NavItem color={themeContext.colors.BUGS} text="BUGS" />
