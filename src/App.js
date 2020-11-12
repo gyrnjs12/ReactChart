d786b736ce6410d67edbfb734ba3a5fc8f75c1fc
HyoKwon <rlagyrnjs12@naver.com>
2020-11-13 01:46:56 +0900

    chart design remodeling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 import NavBar from './components/Nav';
 import ChartTemplate from './components/ChartTemplate';
 // import ChartHead from './components/ChartHead';
 import ChartHead2 from './components/ChartHead2';
+import ChartHead1 from './components/ChartHead';
 import ChartList from './components/ChartList';
 import { ChartProvider } from './components/Provider/ChartProvider.component';
 import theme from './components/theme/theme';
@@ -16,13 +18,16 @@ const GlobalStyle = createGlobalStyle`
 `;
 // 사용하고자 하는 자식 컴포넌트를 밖에서 <Provider>로 감싸주기
 function App() {
+  const isBig = useMediaQuery({
+    query: '(min-width: 1790px)',
+  });
   return (
     <>
       <ChartProvider>
         <GlobalStyle />
         <ThemeProvider theme={theme}>
           <NavBar />
-          <ChartHead2>TEST</ChartHead2>
+          {isBig ? <ChartHead2 /> : <ChartHead1 />}
           <ChartTemplate>
             <ChartList />
           </ChartTemplate>
